f63fd18628ad7d48b2cb05c3d33559a2f8b1b756
Silvan Mosberger <contact@infinisil.com>
2021-02-05 23:33:15 +0100

    hash

--- build.nix
+++ build.nix
@@ -1,7 +1,7 @@
 let
   nixpkgs = fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/340b6780b851bfb93892c8c1819a59d7f1c7ca1a.tar.gz";
-    sha256 = "0000000000000000000000000000000000000000000000000000";
+    sha256 = "0mm5i7g95bk70nfblm8aa0vsz7lddlg1z61zp2kdbm3k6vqym76y";
   };
   pkgs = import nixpkgs {};
 in 
